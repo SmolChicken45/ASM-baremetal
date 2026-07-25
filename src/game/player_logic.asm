b19c0f4df171d2589ed57c48e58d70f054ad8020
SmolChicken45 <98850340+SmolChicken45@users.noreply.github.com>
2026-07-24 21:15:24 -0400

    ajout de la bonne méthode de direction des controles

--- src/game/player_logic.asm
+++ src/game/player_logic.asm
@@ -8,6 +8,13 @@ extern player_data
 
 ; Vitesse en virgule fixe : 1,5 pixels * 256 = 384
 SPEED equ 384
+UP_VALUE equ 9
+LEFT_VALUE equ 7
+DOWN_VALUE equ 10
+RIGHT_VALUE equ 8
+PLAYER_DIRECTION equ 24
+PLAYER_X equ 0
+PLAYER_Y equ 4
 
 section .data
 
@@ -16,50 +23,89 @@ animation_timer    dd 0
 
 section .text
 
-update_player_logic:
 
+; -------------------------------
+; Fonction qui gère le mouvement de Kris
+;
+; Regarde InputState
+;
+; Écrit dans player_data
+; -------------------------------
+update_player_logic:
+    mov r8d, -1
+    mov r9d, -1 
     xor r11d, r11d
 
-.check_left:
-    ; left_value = + 7
-    cmp byte [rel InputState + 7], 0
-    jz .check_right
-    ; MOuvement Gauche
-    mov dword [rel player_data + 24], 1
-    sub dword [rel player_data], SPEED
-    mov r11d, 1
-    jmp .check_vertical
-
-.check_right:
-    ; right_value = + 8
-    cmp byte [rel InputState + 8], 0
-    jz .check_vertical
-    ; Mouvement Droite
-    mov dword [rel player_data + 24], 2
-    add dword [rel player_data], SPEED
-    mov r11d, 1
-
-    ; --- AXE VERTICAL ---
-.check_vertical:
-.check_up:
-    ; up_value = + 9
-    cmp byte [rel InputState + 9], 0
-    jz .check_down
+.check_up_move:
+    cmp byte [rel InputState + UP_VALUE], 0
+    jz .check_down_move
     ; Mouvement Haut
-    mov dword [rel player_data + 24], 3
-    sub dword [rel player_data + 4], SPEED
+    sub dword [rel player_data + PLAYER_Y], SPEED
+    mov r8d, 3
     mov r11d, 1
-    jmp .update_animation
-
-
-.check_down:
-    ; down_value = + 10
-    cmp byte [rel InputState + 10], 0
-    jz .update_animation
+    jmp .check_left_move
+    
+.check_down_move:
+    cmp byte [rel InputState + DOWN_VALUE], 0
+    jz .check_left_move
     ; Mouvement Bas
-    mov dword [rel player_data + 24], 0
-    add dword [rel player_data + 4], SPEED
+    add dword [rel player_data + PLAYER_Y], SPEED
+    mov r8d, 0
     mov r11d, 1
+
+.check_left_move:
+    cmp byte [rel InputState + LEFT_VALUE], 0
+    jz .check_right_move
+    ; Mouvement Gauche
+    sub dword [rel player_data + PLAYER_X], SPEED
+    mov r9d, 1
+    mov r11d, 1
+    jmp .determine_direction
+    
+.check_right_move:
+    cmp byte [rel InputState + RIGHT_VALUE], 0
+    jz .determine_direction
+    ; Mouvement Droite
+    add dword [rel player_data + PLAYER_X], SPEED
+    mov r9d, 2
+    mov r11d, 1
+    
+    
+.determine_direction:
+    test r11d, r11d
+    jz .idle
+    
+    mov eax, dword [rel player_data + PLAYER_DIRECTION]
+
+    cmp eax, r8d
+    je .update_animation
+    
+    cmp eax, r9d
+    je .update_animation
+    
+    ; Direction actuelle n'est plus la bonne
+    
+.try_up:
+    cmp r8d, 3
+    jne .try_left
+    mov dword [rel player_data + PLAYER_DIRECTION], 3
+    jmp .update_animation
+    
+.try_left:
+    cmp r9d, 1
+    jne .try_down
+    mov dword [rel player_data + PLAYER_DIRECTION], 1
+    jmp .update_animation
+    
+.try_down:
+    cmp r8d, 0
+    jne .try_right
+    mov dword [rel player_data + PLAYER_DIRECTION], 0
+    jmp .update_animation
+    
+.try_right:
+    mov dword [rel player_data + PLAYER_DIRECTION], 2
+
 
 .update_animation:
     test r11d, r11d
