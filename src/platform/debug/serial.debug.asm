14964cbf8c4f05161dac43e4aca48da7a4d54dd0
SmolChicken45 <98850340+SmolChicken45@users.noreply.github.com>
2026-07-24 20:01:43 -0400

    Update serial.debug.asm
    Cleanup du code
    Ajout de la fonction serial_write_string

--- src/platform/debug/serial.debug.asm
+++ src/platform/debug/serial.debug.asm
@@ -1,29 +1,58 @@
 [BITS 64]
+global serial_write_string
 global serial_write_qword
 global serial_write_byte
 global serial_init
 
+section .text
+; -------------------------------------
+; Fonction qui initialise le port série
+;
+; Arguments:
+; null
+;
+; Returns:
+; null
+; -------------------------------------
 serial_init:
-    mov dx, 0x3fb
-    mov al, 0x80
+
+    ; Activer le mode configuration du COM1
+    mov dx, 0x3fb   ; Line Control Register
+    mov al, 0x80    ; Divisor Latch Access Bit
     out dx, al
-    mov dx, 0x3f8
-    mov al, 0x03
+    
+    ; Définir la Vitesse
+    mov dx, 0x3f8   
+    mov al, 0x03    ; Puce Série a 115 200 Hz
+    out dx, al      ; 115 200 Hz / 3 = 38 400 bauds
+    
+    ; Configurer et enlever le mode configuration
+    mov dx, 0x3fb   ; Line Control Register
+    mov al, 0x03    ; Format 8-N-1
     out dx, al
-    mov dx, 0x3fb
-    mov al, 0x03
-    out dx, al
+    
     ret
 
+; ---------------------------------------------
+; Fonction qui écrit un Byte dans le port série
+;
+; Arguments:
+; al = byte à envoyer
+;
+; Returns:
+; null
+; ---------------------------------------------
 serial_write_byte:
     push rdx
     push rax
 .wait:
+    ; Attendre que le port série soit prêt
     mov dx, 0x3fd
     in al, dx
     test al, 0x20
     jz .wait
 
+    ; Écrit le byte sur le port série
     pop rax
     mov dx, 0x3f8
     out dx, al
@@ -31,6 +60,15 @@ serial_write_byte:
     pop rdx
     ret
 
+; ---------------------------------------------
+; Fonction qui écrit un Qword dans le port série
+;
+; Arguments:
+; rax = qword à envoyer
+;
+; Returns:
+; null
+; ---------------------------------------------
 serial_write_qword:
 
     push rbx
@@ -66,3 +104,41 @@ serial_write_qword:
     pop rbx
 
     ret
+    
+
+; ---------------------------------------------
+; Fonction qui écrit une suite de caractères dans le port série
+;
+; Arguments:
+; rax = ptr vers le string qui est fini par un zéro
+; rcx = nombre maximal de caractère à envoyer
+; Returns:
+; null
+; ---------------------------------------------
+serial_write_string:
+    push rbx
+    push rcx
+    
+    mov rbx, rax    ; pointeur du char courrant
+    
+.loop:
+    ; Test nbre caractère max
+    test rcx, rcx
+    jz .done
+    
+    ; Test de fin de caractère (0x00)
+    mov al, [rbx]
+    test al, al
+    jz .done
+    
+    call serial_write_byte
+    
+    inc rbx
+    dec rcx
+    jmp .loop
+    
+.done:
+    pop rcx
+    pop rbx
+    ret
+    
